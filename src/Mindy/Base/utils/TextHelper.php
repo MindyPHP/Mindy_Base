639a010a8d0d0415de2342d9b39eeeba84f3560c
Falaleev Maxim <max@studio107.ru>
2014-06-11 13:35:24 +0400

    big refactoring

--- src/Mindy/Base/utils/TextHelper.php
+++ src/Mindy/Base/utils/TextHelper.php
@@ -51,4 +51,9 @@ class TextHelper
         }
         return $text;
     }
+
+    public static function mbUcfirst($word)
+    {
+        return mb_strtoupper(mb_substr($word, 0, 1, 'UTF-8'), 'UTF-8') . mb_substr(mb_convert_case($word, MB_CASE_LOWER, 'UTF-8'), 1, mb_strlen($word), 'UTF-8');
+    }
 }
