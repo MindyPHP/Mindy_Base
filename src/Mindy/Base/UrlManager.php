9ced43c56dd2d0eb2fb86ee64ad7d35717927ca1
Falaleev Maxim <max@studio107.ru>
2014-07-17 18:39:19 +0400

    Enable trailingSlash default

--- src/Mindy/Base/UrlManager.php
+++ src/Mindy/Base/UrlManager.php
@@ -11,7 +11,7 @@ class UrlManager extends AuraMap
 {
     public $urlsAlias = 'application.config.urls';
 
-    public $trailingSlash = false;
+    public $trailingSlash = true;
 
     public function __construct()
     {
