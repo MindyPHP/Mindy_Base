9087b8b86fc04e1fbadacad5d88b4af8fd542c62
Falaleev Maxim <max@studio107.ru>
2014-10-16 20:48:24 +0400

    Refact

--- src/Mindy/Base/Module.php
+++ src/Mindy/Base/Module.php
@@ -2,10 +2,60 @@
 
 namespace Mindy\Base;
 
-abstract class Module extends BaseModule
+class Module extends BaseModule
 {
     public function getVersion()
     {
         return '1.0';
+    }
+
+    /**
+     * Return array for MMenu {$see: MMenu} widget
+     * @return array
+     */
+    public function getMenu()
+    {
+        return [];
+    }
+
+    /**
+     * Return array of mail templates and his variables
+     * @return array
+     */
+    public function getMailTemplates()
+    {
+        return [];
+    }
+
+    /**
+     * Install module
+     * @void
+     */
+    public function install()
+    {
+    }
+
+    /**
+     * Uninstall module. Delete tables from database.
+     * @void
+     */
+    public function uninstall()
+    {
+    }
+
+    /**
+     * Upgrade module to new version. Run migrations, update sql.
+     * @void
+     */
+    public function upgrade()
+    {
+    }
+
+    /**
+     * Downgrade module to old version. Delete tables from database if need.
+     * @void
+     */
+    public function downgrade()
+    {
     }
 }
